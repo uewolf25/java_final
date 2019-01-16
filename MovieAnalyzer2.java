5e1ccd63d0b6f7abf3dc7fcc0f77044c3e8933ae
wolf <nyan643.ym25mm10@gmail.com>
2019-01-17 04:18:36 +0900

    step2 complete

--- MovieAnalyzer2.java
+++ MovieAnalyzer2.java
@@ -2,20 +2,25 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Map;
 import java.util.TreeMap;
 
-public class MovieAnalyzer1{
+public class MovieAnalyzer2{
     TreeMap<Integer, Integer> map; //①Key=年代, Value=回数
 
     void run(String[] args) throws IOException {
         this.initialize(); //マップの初期化
-        for (String item : args) {
-            //ファイルの読み込み
-            this.readFile( new File(item) );
-            //結果を出力
-            this.printTreeMap();
-        }
+        BufferedReader in = null;
+        //引数有る無しで読み込み方法を変える
+        in = this.judgeBinary(args, in);
+
+        //ファイルの読み込み
+        this.readFile(in);
+        //ファイルストリームを閉じる
+        in.close();
+        //結果を出力
+        this.printTreeMap();
     }
 
     /* マップの初期化 */
@@ -23,20 +28,32 @@ public class MovieAnalyzer1{
         this.map = new TreeMap<>();
     }
 
+    /* 引数有る無しで読み込み方法を変える。 */
+    BufferedReader judgeBinary(String[] args, BufferedReader in) throws IOException {
+        if(args.length == 0){ 
+            //標準入力を受け付ける
+            in = new BufferedReader( new InputStreamReader(System.in) );
+        } else{ 
+            //ステップ１と同じ作業をする。
+            for (String item : args) {
+                in = new BufferedReader( new FileReader( item ) );
+            }
+        }
+        return in;
+    }
+
     /* ファイルの読み込み */
-    void readFile(File file) throws IOException {
-        BufferedReader in = new BufferedReader( new FileReader(file) );
+    void readFile( BufferedReader in) throws IOException {
         String line;
         while( ( line = in.readLine() ) != null ){
             //公開年の回数を調べ、格納＆初期化。
             this.frequencyCount(line);
         }
-        in.close();
     }
 
     /* 公開年の回数を調べ、格納＆初期化 */
     void frequencyCount(String line) throws IOException {
-        String[] items = line.split(",");
+        String[] items = line.split(","); //文字の切り離し
         Integer releseYear = Integer.valueOf( items[0] ); //文字列から数値への変換
         Integer count = map.get(releseYear);
         if(count == null){
@@ -54,8 +71,9 @@ public class MovieAnalyzer1{
         }
     }
 
+
     public static void main(String[] args) throws IOException {
-        MovieAnalyzer1 pra1 = new MovieAnalyzer1();
-        pra1.run(args);
+        MovieAnalyzer2 pra2 = new MovieAnalyzer2();
+        pra2.run(args);
     }
 }
