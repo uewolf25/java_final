22a26e55418b8ab90b0dac5fefa8011c14d033d1
wolf <nyan643.ym25mm10@gmail.com>
2019-01-22 02:40:40 +0900

    step3 complete

--- Cast.java
+++ Cast.java
@@ -1,16 +1,28 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Cast{
-    String name;
-    Integer count = 0;
-    ArrayList<String> genres = new ArrayList<>();
+    /* このクラス内でリストや変数を保存しておき、ここで出力する。 */
 
-    void update(String genreString){
-        String items = genreString.split("[,/] *"); // ,/で区切る。
+    String name = ""; //役者名
+    Integer count = 0; //出演数
+    ArrayList<String> genres = new ArrayList<>(); //ジャンル
+
+    void update(String name, String genreString){
+        this.name = name;
+        String[] items = genreString.split("[,/] *"); // ,/で区切る。
+        //コンマで区切ったジャンルを１つずつリストに格納。
+        ArrayList<String> genresList = new ArrayList<>( Arrays.asList(items) );
+        count++; //出演数の加算
+        for(Integer i = 0; i < genresList.size(); i++){
+            if( !genres.contains( genresList.get(i) ) ){
+                genres.add( genresList.get(i) ); //なかったら追加
+            }
+        }
     }
 
     String string(){
-        return String.format("適切な文字列に置き換える");
+        return String.format("%s,%d,%s%n", name, count, genres);
     }
 
 }
